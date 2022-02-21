6d3bd2128c39df13529000b6881695a91691e091
TRIAEIOU <triaeiou@protonmail.com>
2022-02-21 21:33:21 +0100

    Refactor HTML, add configurable expose position, add reverse expose, scrolling fix.

--- README.md
+++ README.md
@@ -6,22 +6,25 @@ Flexible cloze is an Anki addon (https://ankiweb.net/shared/info/1632356464) for
 **ALL CREDIT FOR INNOVATION GOES TO TRGANKI AND RISINGORANGE**
 
 <img src="https://aws1.discourse-cdn.com/standard11/uploads/anki2/original/2X/c/cd79af177ab8298f861868740a357429984f4a59.jpeg" height="300">
+
 <img src="https://aws1.discourse-cdn.com/standard11/uploads/anki2/original/2X/f/f00bd7c90b8f0163a91aa96e9abd75a1666b53b6.jpeg" height="300">
 
 ## General
 - Active cloze: Cloze(s) with the current ordinal, i.e. the cloze(s) that should be answered. To change styling of these change (or better yet override) "fcz-active" class in "Styling" of the card template.
 - Inactive cloze: Cloze(s) that are not the current ordinal, i.e. the cloze(s) that should not be answered. To change styling of these change (or better yet override) "fcz-inactive" class in "Styling" of the card template.
-- Exposed cloze: Cloze(s) that when inactive (see above) will always be in "shown" state on the front side. Mark a cloze as exposed in the editor by making the first character an exclamation mark (configurable character/string) (e.g. "{{c1::!This will be displayed on the front when the cloze ordinal is not 1}}"). To change styling of these change (or better yet override) "fcz-expose" class in "Styling" of the card template.
+- Exposed cloze: Cloze(s) that when inactive (see above) will always be in "shown" state on the front side. Mark a cloze as exposed in the editor by making the first character an exclamation mark (configurable character/string) (e.g. "{{c1::!This will be displayed on the front when the cloze ordinal is not 1}}"). Configurable position of the expose position (pre: !{{c1::content}}/begin (default): {{c1::!content}}/end: {{c1::content!}}/post: {{c1::content}}!) to allow use with {{type:cloze:Text}}. Configurable to reverse the expose status, i.e. all inactive clozes are exposed (similar to "regular" Anki cloze functonality) and those marked as "expose" will be hidden. To change styling of these change (or better yet override) "fcz-expose" class in "Styling" of the card template.
 - Scrolling: Configurable scrolling to active close behaviour ("min" scrolls as little as possible to get active cloze(s) into view, "center" centers the active clozes in the vindow).
 - Styling of different elements (e.g. "I want the answer to be displayed inline rather than in a block") can easily be configured in the "Styling" section of the card template (which is now fairly documented with descriptions).
 - To facilitate end user modifications of the layout, style and function of the template updates will come with a dialog allowing the user to determine which parts to update. A temporary backup (will be overwritten on next update) of the current template will be created in the add-on folder.
 - Changes will mainly be made inside "FCZ BEGIN/END" tags, which in turn is divided into functionality and styling allowing the user to avoid overwriting the styling part on update.
 - Configuration is made in the "Styling" section of the card template under the ".fcz-config" heading:
-<pre><code>/* FLEXIBLE CLOZE CONFIGURATION======================================= */
+<code>/* FLEXIBLE CLOZE CONFIGURATION======================================= */
 .fcz-config {
     --cloze-element: "div";                 /* What HTML element the clozes will be wrapped in */
-    --expose: "#";                          /* String to mark clozes to be exposed when inactive */
-    --scroll: "center";                     /* How to scroll to current cloze, valid values are "min" and "center" */
+    --expose: "!";                          /* String to mark clozes to be unhidden when inactive */
+    --expose-pos: "begin";                  /* Position of expose character/string in cloze (pre, begin, end, post) */
+    --reverse-expose: "false";              /* "true" exposes all inactive clozes by default (reversing the use of the expose char/string) */
+    --scroll: "center";						/* How to scroll to current cloze, valid values are "min" and "center" */
     --inactive-prompt: "";                  /* Text to display (if any) on inactive hidden clozes */
     --active-prompt: "";                    /* Text to display (if any) on active hidden clozes */
     --key-next-cloze: "j";                  /* Keyboard shortcut to iterate forward */
@@ -32,29 +35,30 @@ Flexible cloze is an Anki addon (https://ankiweb.net/shared/info/1632356464) for
     --iteration-hides-previous: "true";     /* false leaves cloze open when cycling forward/backward */
     --show-all-on-back: "false";            /* false initially hides inactive clozes on back */
     --show-additional-on-back:  "true";     /* false initially hides the additional fields on back */
-}</code></pre>
+}</code>
 - Clicking an active cloze on the front side will cycle it between hint (if there is one) and show.
 - Clicking an inactive cloze on the front side will cycle it through hide - hint - show.
 - A suggestion is using show new siblings in order / no same day spacing(randomization) for new siblings (https://ankiweb.net/shared/info/268644742) when writing notes that are "sequential" in nature. I.e. when the later clozes in the note requires knowing the earlier clozes. This makes the clozes be presented in ascending order on new cards (which is when it needs to be ordered). Once learned they will not be ordered (or even on the same day depending on how well/fast you learn the different clozes) but then you already "know the preceding clozes".
 - There is an optional "show all" button (styleable in class .fcz-show-all-btn), note that:
   - The button is set to display: none in default configuration, you have to set it to display: inline on the Styling page of the cards dialog to get it to show.
-<pre><code>/* ANSWER SIDE STYLING ============================================== */
+<code>/* ANSWER SIDE STYLING ============================================== */
 /* Show all button/bar styling (and if visible or not) */
 .fcz-show-all-btn
 { display: inline;  background-color: #465A65; color: white; text-align: center; text-transform: uppercase;
-font-size: 15px; font-weight: bold; padding: 5px; border-bottom: 1px solid white; }</code></pre>
+font-size: 15px; font-weight: bold; padding: 5px; border-bottom: 1px solid white; }</code>
   If you are updating from a previous version of the template and choose to not "overwrite all" (overwriting any personal changes you have made) you will have to manually add the actual button on the front as this requires changing the HTML outside the FCZ BEGIN/END tags. Insert `<div id="fcz-show-all" class="fcz-show-all-btn" style="cursor: pointer;" onclick="fcz_toggle_all()">Show all</div>` just after the closing div-tag of the `<div id="fcz-additional" style="z-index: 2;">` (have a look in the add-on directory in fcz-front.html).
 - https://github.com/TRIAEIOU/Flexible-cloze
 
 ## Regarding styling
 As there have been a few questions regarding the default styling of the template not looking like "regular Anki clozes". You can have the clozes display however you want by adjusting the CSS on the "Styling" page of the "Cards" dialog. To achieve the "regular Anki cloze styling":
 In the "FLEXIBLE CLOZE CONFIGURATION" section set:
-<pre><code>--cloze-element: "span";
+<code>--cloze-element: "span";
 --inactive-prompt: "[...]";
---active-prompt: "[...]";</code></pre>
+--active-prompt: "[...]";</code>
 Replace all the content under the "CLOZE STYLING" section with `.fcz-active { color: blue; font-weight: bold; }`. If by chance I am little off on the font-weight you can fine-tune it by starting at 400 which is normal font-weight and going upward (900 would be "very bold"). Similarly if by chance the blue nuance is off you can insert the correct RGB instead, e.g. #0000FF.
 
 <img src="https://aws1.discourse-cdn.com/standard11/uploads/anki2/original/2X/1/19e94cbb8a36c790df2b0595d617820ab8ece13e.jpeg" height="300">
+
 <img src="https://aws1.discourse-cdn.com/standard11/uploads/anki2/original/2X/2/20433455dd0c9ee1c2c6644e7c8e69fb2c3beec3.jpeg" height="300">
 
 ## Main difference from the earlier mentioned add-ons
@@ -82,8 +86,9 @@ There is effectively no add-on, it's all JavaScript (and HTML/CSS) and runs 100%
 - 2021-11-13: Added selective update dialog inc. temporary backup of previous template. Added exposed cloze functionality (see above). Added option to iterate inactive clozes (see above). Added option to always start next/previous iteration from top (see above). Split FCZ BEGIN/END content into FUNCTIONALITY and STYLING for selective updates. Added symbol legend at bottom. Refactoring of code.
 - 2021-11-15: Added functionality/options to initially show or hide inactive clozes (--show-all-on-back) and additional fields (--show-additional-on-back). Added a "show all button" (stylable under .fcz-show-all-btn-class).
 - 2021-12-15: Custom scroll logic implemented, it should now automatically scroll to fit as many active clozes as possible when required (scrolling as little as possible but with a single line margin). Minor code refactoring.
-- 2021-12-26: Adjustment of scroll logic to facilitate user custom card layout. If not using (or not updating to the latest version of) the "default" Flexible Cloze template the <div> that should be scrolled needs to have the "fcz-scroll-area" class (the scroll logic is applied to the first element with the fcz-scroll-area class).
+- 2021-12-26: Adjustment of scroll logic to facilitate user custom card layout. If not using (or not updating to the latest version of) the "default" Flexible Cloze template the &lt;div&gt; that should be scrolled needs to have the "fcz-scroll-area" class (the scroll logic is applied to the first element with the fcz-scroll-area class).
 - 2022-01-01: Scroll logic bug fix.
 - 2022-01-06: 2022-01-05: Scroll logic bug fix (again), restructuring of HTML on "Front" to correct scroll bugs.
 - 2022-02-11: Made exposed cloze character/string configurable (to allow character/string that does not cause TTS to kick in), made scroll behaviour configurable (as little as possible or center active clozes).
 - 2022-02-13: Bug fixes, refactoring. Moved {{FrontSide}} on back of card inside FCZ functionality tag (required for current design).
+- 2022-02-21: Refactor front side HTML for better compatibility with addons that inject HTML on the card, such as Remaining time for Anki 21 (update requires migration to new HTML model). Further enhancement of scrolling (await loading of images on image heavy cards). Added configurable position of expose character/string: pre/begin (default)/end/post cloze tags for better compatibility with type-in-the-answer (avoid having to type the expose character/string). Added configurable reverse-expose: all inactive clozes per default exposed (similar to "regular" Anki clozes), expose character/string hides them.
--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import re
 import os
 from typing import Dict, Tuple
-from aqt import mw
+from aqt import mw, QPushButton, QMessageBox
 from aqt.qt import *
 from anki.models import NotetypeDict
 from anki.hooks import addHook
@@ -70,6 +70,9 @@ def build_mix_page(pre: str, ctag: str, ntag: str, post: str) -> str:
 def update():
     (nfront, nback, ncss) = load_file()
     model = mw.col.models.by_name(FCZ_NAME)
+
+    ###################################################################
+    # Initial install, not updgrade
     if not model:
         mw.col.models.add_dict({"vers": [], "name": FCZ_NAME, "tags": [], "did": 1, "usn": -1, "flds": [
                 {"name": "Title", "media": [], "sticky": False, "rtl": False, "ord": 0,  "font": "Arial", "size": 20},
@@ -89,84 +92,104 @@ def update():
             \begin{document}
             """, "latexPost": r"\end{document}", "type": 1, "id": 0, "css": ncss})
         clear_update()
-        return
 
-    cfront = model["tmpls"][0]["qfmt"]
-    cback = model["tmpls"][0]["afmt"]
-    ccss = model["css"]
-    msg_box = QMessageBox(mw)
-    msg_box.setWindowTitle("Flexible Cloze update")
-    msg_box.setText(f"""<div style="text-align: left;">Flexible Cloze update may include both changes in functionality and styling.
-    <ul><li>Select "All" to completely overwrite the Flexible Cloze template, erasing all personal modifications (temporary backups created in add-on folder).</li>
-    <li>Select "Functionality & styling" to update both functionality and styling, overwriting any personal changes inside "FCZ 1632356464 BEGIN/END" tags (temporary backups created in add-on folder).</li>
-    <li>Select "Functionality only" to only install functionality updates, overwriting any personal changes inside "FUNCTIONALITY BEGIN/END" tags but leaving content inside "STYLE BEGIN/END" tags intact (temporary backups created in add-on folder).</li>
-    <li>Select "None" to manually implement updates from the source files in the add-on directory (fcz-front.html, fcz-back.html, fcz.css).</li>
-    <li>Select "Cancel" to delay update until next Anki start (for instance, to copy a personalized template to another name).</li></ul></div>""")
-    ALL = 0
-    msg_box.addButton(QPushButton('All'), QMessageBox.YesRole)
-    FUNC_STYLE = 1
-    msg_box.addButton(QPushButton('Functionality && Styling'), QMessageBox.YesRole)
-    FUNC = 2
-    msg_box.addButton(QPushButton('Functionality only'), QMessageBox.YesRole)
-    NONE = 3
-    msg_box.addButton(QPushButton('None'), QMessageBox.NoRole)
-    CANCEL = 4
-    msg_box.addButton(QPushButton('Cancel'), QMessageBox.RejectRole)
-    scope = msg_box.exec()
+    ###################################################################
+    # Existing install - upgrade
+    else: 
+        # Parse existing install
+        cfront = model["tmpls"][0]["qfmt"]
+        cback = model["tmpls"][0]["afmt"]
+        ccss = model["css"]
 
-    if scope == CANCEL:
-        return
+        cfront_parts = parse_tag(cfront, FCZ_BTAG, FCZ_ETAG)
+        cback_parts = parse_tag(cback, FCZ_BTAG, FCZ_ETAG)
+        ccss_parts = parse_tag(ccss, FCZ_BTAG, FCZ_ETAG)
 
-    if scope == NONE:
-        clear_update()
-        return
-    
-    cfront_parts = parse_tag(cfront, FCZ_BTAG, FCZ_ETAG)
-    cback_parts = parse_tag(cback, FCZ_BTAG, FCZ_ETAG)
-    ccss_parts = parse_tag(ccss, FCZ_BTAG, FCZ_ETAG)
-    if not (cfront_parts and cback_parts and ccss_parts):
-        ans = QMessageBox.critical(mw, 'FCZ update error - overwrite current template?', f'''<div style="text-align: left">Failed to install update, current "Flexible cloze" card template corrupt.
-        <ul><li>Press "Yes" to completely overwrite current template (temporary backups created in add-on folder).</li>
-        <li>Press "No" to manually correct template from add-on folder.</li>
-        <li>Press "Cancel" to delay update attempt (the update dialog will resume next Anki restart).</li></ul></div>''', QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
-        if ans == QMessageBox.Yes:
+        # Failed to parse existing install
+        if not (cfront_parts and cback_parts and ccss_parts):
+            ans = QMessageBox.critical(mw, 'FCZ update error - overwrite current template?', f'''<div style="text-align: left">Current "Flexible cloze" card template corrupt.
+            <ul><li>Press "Yes" to completely overwrite current template (temporary backups created in add-on folder).</li>
+            <li>Press "No" to manually correct template from add-on folder.</li>
+            <li>Press "Cancel" to delay update attempt (the update dialog will resume next Anki restart).</li></ul></div>''', QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
+            if ans == QMessageBox.Yes:
+                model["tmpls"][0]["qfmt"] = nfront
+                model["tmpls"][0]["afmt"] = nback
+                model["css"] = ncss
+                mw.col.models.update(model)
+                backup_file(cfront, cback, ccss)
+                clear_update()
+            elif ans == QMessageBox.No:
+                clear_update()
+        
+        # Existing install valid
+        else:
+            nfront_parts = parse_tag(nfront, FCZ_BTAG, FCZ_ETAG)
+            nback_parts = parse_tag(nback, FCZ_BTAG, FCZ_ETAG)
+            ncss_parts = parse_tag(ncss, FCZ_BTAG, FCZ_ETAG)
+
+            update_msg = f"""Flexible Cloze update may include both changes in functionality and styling.
+            <ul><li>Select "All" to completely overwrite the Flexible Cloze template, erasing all personal modifications (temporary backups created in add-on folder).</li>
+            <li>Select "Functionality & styling" to update both functionality and styling, overwriting any personal changes inside "FCZ 1632356464 BEGIN/END" tags (temporary backups created in add-on folder).</li>
+            <li>Select "Functionality only" to only install functionality updates, overwriting any personal changes inside "FUNCTIONALITY BEGIN/END" tags but leaving content inside "STYLE BEGIN/END" tags intact (temporary backups created in add-on folder).</li>
+            <li>Select "None" to manually implement updates from the source files in the add-on directory (fcz-front.html, fcz-back.html, fcz.css).</li>
+            <li>Select "Cancel" to delay update until next Anki start (for instance, to copy a personalized template to another name).</li></ul>"""
+            
+            #####################################################################
+            # Version specific updates
+            ver_msg = "" # Version specific message(s) as <li>'s
+
+            # On back strip out {{FrontSide}} from outside tags from < 1.7 versions
+            if float(cback_parts['ver']) < 1.7:
+                cback_parts['pre'] = cback_parts['pre'].replace('{{FrontSide}}', '')
+                cback_parts['post'] = cback_parts['post'].replace('{{FrontSide}}', '')
+
+            # Inform of front HTML change in 1.8
+            if float(cback_parts['ver']) < 1.8:
+                ver_msg += '<li>Version migration: Front side HTML has been refactored to be compatible with other addons, the current version will need to be migrated. Chose "All" to completely overwrite current version (including personal styling) or migrate front changes manually (HTML before the FCZ BEGIN tag) from fcz-front.html in addon source directory.</li>'
+            
+            if ver_msg:
+                update_msg = f'''{update_msg}<br><b>IMPORTANT - VERSION SPECIFIC UPDATES</b><ul>{ver_msg}</ul>'''
+
+
+            # Update dialog
+            msg_box = QMessageBox(mw)
+            msg_box.setWindowTitle("Flexible Cloze update")
+            msg_box.setText(f'<div style="text-align: left;">{update_msg}</div>')
+
+            ALL = 0
+            msg_box.addButton(QPushButton('All'), QMessageBox.YesRole)
+            FUNC_STYLE = 1
+            msg_box.addButton(QPushButton('Functionality && Styling'), QMessageBox.YesRole)
+            FUNC = 2
+            msg_box.addButton(QPushButton('Functionality only'), QMessageBox.YesRole)
+            NONE = 3
+            msg_box.addButton(QPushButton('None'), QMessageBox.NoRole)
+            CANCEL = 4
+            msg_box.addButton(QPushButton('Cancel'), QMessageBox.RejectRole)
+            scope = msg_box.exec()
+
+            if scope == CANCEL:
+                return
+
+            if scope == NONE:
+                clear_update()
+                return
+
+            if scope == FUNC:
+                nfront = build_mix_page(cfront_parts['pre'], cfront_parts['tag'], nfront_parts['tag'], cfront_parts['post'])
+                nback = build_mix_page(cback_parts['pre'], cback_parts['tag'], nback_parts['tag'], cback_parts['post'])
+                ncss = build_mix_page(ccss_parts['pre'], ccss_parts['tag'], ncss_parts['tag'], ccss_parts['post'])
+            elif scope == FUNC_STYLE:
+                nfront = f"{cfront_parts['pre']}{nfront_parts['tag']}{cfront_parts['post']}"
+                nback = f"{cback_parts['pre']}{nback_parts['tag']}{cback_parts['post']}"
+                ncss = f"{ccss_parts['pre']}{ncss_parts['tag']}{ccss_parts['post']}"
+
             model["tmpls"][0]["qfmt"] = nfront
             model["tmpls"][0]["afmt"] = nback
             model["css"] = ncss
             mw.col.models.update(model)
             backup_file(cfront, cback, ccss)
             clear_update()
-            return
-        elif ans == QMessageBox.No:
-            clear_update()
-            return
-        else:
-            return
-
-    nfront_parts = parse_tag(nfront, FCZ_BTAG, FCZ_ETAG)
-    nback_parts = parse_tag(nback, FCZ_BTAG, FCZ_ETAG)
-    ncss_parts = parse_tag(ncss, FCZ_BTAG, FCZ_ETAG)
-
-    # On back strip out {{FrontSide}} from outside tags from old versions
-    if float(cback_parts['ver']) < 1.7:
-        cback_parts['pre'] = cback_parts['pre'].replace('{{FrontSide}}', '')
-        cback_parts['post'] = cback_parts['post'].replace('{{FrontSide}}', '')
-
-    if scope == FUNC:
-        nfront = build_mix_page(cfront_parts['pre'], cfront_parts['tag'], nfront_parts['tag'], cfront_parts['post'])
-        nback = build_mix_page(cback_parts['pre'], cback_parts['tag'], nback_parts['tag'], cback_parts['post'])
-        ncss = build_mix_page(ccss_parts['pre'], ccss_parts['tag'], ncss_parts['tag'], ccss_parts['post'])
-    elif scope == FUNC_STYLE:
-        nfront = f"{cfront_parts['pre']}{nfront_parts['tag']}{cfront_parts['post']}"
-        nback = f"{cback_parts['pre']}{nback_parts['tag']}{cback_parts['post']}"
-        ncss = f"{ccss_parts['pre']}{ncss_parts['tag']}{ccss_parts['post']}"
-
-    model["tmpls"][0]["qfmt"] = nfront
-    model["tmpls"][0]["afmt"] = nback
-    model["css"] = ncss
-    mw.col.models.update(model)
-    backup_file(cfront, cback, ccss)
-    clear_update()
 
 
 if os.path.isfile(os.path.join(os.path.dirname(__file__), "update")):
